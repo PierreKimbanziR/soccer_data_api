fece82425ea819e700111dd9be70e8257aac2ffb
PierreKimbanziR <kimbanzip@gmail.com>
2021-08-04 19:45:27 +0200

    add class based views

--- soccer_api/players/views.py
+++ soccer_api/players/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+from players.models import PlayerSeasonStats
+from players.serializers import PlayerSeasonStatsSerializer
+
+
+class PlayerSeasonsStatsList(generics.ListAPIView):
+    queryset = PlayerSeasonStats.objects.all()
+    serializer_class = PlayerSeasonStatsSerializer
+    name = "player-stats-list"
+
+class PlayerSeasonStatsDetail(generics.RetrieveUpdateAPIView):
+    queryset = PlayerSeasonStats.objects.all()
+    serializer_class = PlayerSeasonStatsSerializer
+    name = "player-stats-detail"
+
--- soccer_api/teams/serializers.py
+++ soccer_api/teams/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import TeamSeasonStats
 
 
-class TeamSeasonStatsSerializer(serializers.ModelSerializer):
+class TeamSeasonStatsSerializer(serializers.HyperlinkedModelSerializer):
+    players = serializers.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name="player-stats-detail"
+    )
+
     class Meta:
         model = TeamSeasonStats
         fields = "__all__"
