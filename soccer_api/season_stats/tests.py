29e69003653ccce28da589ee3a590ce309aa93ba
PierreKimbanziR <kimbanzip@gmail.com>
2021-08-09 21:41:55 +0200

    add players tests

--- soccer_api/season_stats/tests.py
+++ soccer_api/season_stats/tests.py
@@ -170,5 +170,163 @@ class TeamsSeasonStatsTests(APITestCase):
         assert get_response.status_code == status.HTTP_200_OK
         assert get_response.data["team_name"] == new_team_name
 
+class PlayerSeasonStatsTests(APITestCase):
+    
+    def create_user(self):
+        fk = Faker()
+        username = fk.first_name()+fk.last_name()
+        email = f'{username}@gmail.com'
+        password = "testtest1234"
+        user = User.objects.create_user(username, email, password)
+        self.client.login(username=username, password=password)
 
+    def post_player_stats(self, player_name):
+        url = reverse(views.PlayerSeasonsStatsList.name)
+        data = {
+            "player_name": player_name,
+            "team_name": "",
+            "nation": "ar ARG",
+            "position": "GK",
+            "age": 27,
+            "matches_played": 38,
+            "starts": 38,
+            "minutes_played": 3420,
+            "minutes_played_by_90": 38.0,
+            "goals_scored": 0,
+            "assists": 0,
+            "non_penalty_goals": 0,
+            "penalty_goals": 0,
+            "penalty_attempted": 0,
+            "yellow_cards": 1,
+            "red_cards": 0,
+            "goals_per_90": 0.0,
+            "assists_per_90": 0.0,
+            "gls_asts_per_90": 0.0,
+            "non_penalty_goals_per_90": 0.0,
+            "non_penalty_goals_ast_per_90": 0.0,
+            "xG": 0.0,
+            "npxG": 0.0,
+            "xA": 0.2,
+            "npxG_xA": 0.2,
+            "xG_per_90": 0.0,
+            "xA_per_90": 0.01,
+            "xG_xA_per_90": 0.01,
+            "npxG_per_90": 0.0,
+            "npxG_xA_per_90": 0.01
+        }
+        self.create_user()
+        response = self.client.post(url, data, format="json")
+        return response
+
+    def test_post_player(self):
+        team_name = "FCpierre"
+        response = self.post_player_stats(team_name)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_post_and_get_single_player_stats(self):
+        """
+        Ensure we create new player_stats instance and then retrieve it
+        """
+        player_name = "Jean_Luc_Duduche"
+        response = self.post_player_stats(player_name)
+        print("Pk {0}".format(PlayerSeasonStats.objects.get().team_name))
+        assert response.status_code == status.HTTP_201_CREATED
+        assert PlayerSeasonStats.objects.count() == 1
+        assert PlayerSeasonStats.objects.get().player_name == player_name
+
+    def test_post_existing_team_name(self):
+        """
+        Ensure we cannot create mutiple teams with the same name
+        """
+        player_name = "axel_foley"
+        response1 = self.post_player_stats(player_name)
+        assert response1.status_code == status.HTTP_201_CREATED
+        
+        response2 = self.post_player_stats(player_name)
+        assert response2.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_filtering_team_name(self):
+        """
+        Ensure we can filter team by team_name
+        """
+        player_name1 = "Freacois_Venant"
+        player_name2 = "Jean_Tand"
+        self.post_player_stats(player_name1)
+        self.post_player_stats(player_name2)
+        filter_by_name = {"player_name": player_name1}
+        url = "{0}?{1}".format(
+            reverse(views.PlayerSeasonsStatsList.name), urlencode(filter_by_name)
+        )
+        print(url)
+        response = self.client.get(url, format="json")
+        print(response)
+        assert response.status_code == status.HTTP_200_OK
+        # Make sure we receive  only one element in the response
+        assert response.data["count"] == 1
+        assert response.data["results"][0]["player_name"] == player_name1
+
+    def test_retrieving_teams_collection(self):
+        player_name = "Gregory_Lebel"
+        self.post_player_stats(player_name)
+        url = reverse(views.PlayerSeasonsStatsList.name)
+        response = self.client.get(
+            url,
+            format="json",
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"][0]["player_name"] == player_name
+
+    def test_update_team_data(self):
+        player_name = "Cristiano_Ronaldo"
+        response = self.post_player_stats(player_name)
+        url = reverse(
+            views.PlayerSeasonStatsDetail.name, args={response.data["player_name"]}
+        )
+        print(url)
+        updated_player_name = "Cristiano_Ronaldo_Jr"
+        data = {
+            "player_name": updated_player_name,
+            "team_name": "",
+            "nation": "ar ARG",
+            "position": "GK",
+            "age": 27,
+            "matches_played": 38,
+            "starts": 38,
+            "minutes_played": 3420,
+            "minutes_played_by_90": 38.0,
+            "goals_scored": 0,
+            "assists": 0,
+            "non_penalty_goals": 0,
+            "penalty_goals": 0,
+            "penalty_attempted": 0,
+            "yellow_cards": 1,
+            "red_cards": 0,
+            "goals_per_90": 0.0,
+            "assists_per_90": 0.0,
+            "gls_asts_per_90": 0.0,
+            "non_penalty_goals_per_90": 0.0,
+            "non_penalty_goals_ast_per_90": 0.0,
+            "xG": 0.0,
+            "npxG": 0.0,
+            "xA": 0.2,
+            "npxG_xA": 0.2,
+            "xG_per_90": 0.0,
+            "xA_per_90": 0.01,
+            "xG_xA_per_90": 0.01,
+            "npxG_per_90": 0.0,
+            "npxG_xA_per_90": 0.01
+        }
+        patch_response = self.client.patch(url, data, format="json")
+        assert patch_response.status_code == status.HTTP_200_OK
+        assert patch_response.data["player_name"] == updated_player_name
+
+    def test_get_team_by_name(self):
+        player_name = "Frederic_Lemaitre"
+        response = self.post_player_stats(player_name)
+        url = reverse(
+            views.PlayerSeasonStatsDetail.name, None, {response.data["player_name"]}
+        )
+        get_response = self.client.get(url, format="json")
+        assert get_response.status_code == status.HTTP_200_OK
+        assert get_response.data["player_name"] == player_name
 # Create your tests here.
